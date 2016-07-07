8264e2ce3d5a7ebcbdce5dfc519c37afc163cf5a
brian-menzies <brian.menzies@orasi.com>
2016-07-07 10:42:31 -0400

    Changed Comment message in EmailList file to be more informative

--- Helpers/EmailList.py
+++ Helpers/EmailList.py
@@ -1,7 +1,9 @@
 import unittest
 
 class EmailList(unittest.TestResult):
-    #This serves as the place for the email addresses that will
+    #This serves as the place for the email addresses that will receive the
+    #Results from teh tests run and can be overwritten by a Jenkins project
+    #Post-Build process and inherit the projects' file
 
     email_list =  {
         "brian.menzies@orasi.com"
